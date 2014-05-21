7b65d17f3f1d77038cd7c5ecd08283525a42808d
Jens-Ole Graulund <jensole@graulund.net>
2014-05-21 13:52:52 +0200

    Target openshift mongoDB database

--- server.js
+++ server.js
@@ -1,159 +1,74 @@
-#!/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
+var mosca = require('mosca')
+    , Auth0Mosca = require('./extensions/auth0Mosca')
+    , timeSeriesStore = require('ts-store')
+    , MongoClient = require('mongodb').MongoClient
+    , config = require('./config.json');
 
+//provide a sensible default for local development
+var mqtt_mongodb_connection_string = config.mqtt.dbConnection + config.mqtt.db;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+    mqtt_mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + config.mqtt.db;
+}
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+//provide a sensible default for local development
+var ts_mongodb_connection_string = config.mqtt.dbConnection + config.tsstore.db;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+    ts_mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + config.tsstore.db;
+}
 
-    //  Scope.
-    var self = this;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || config.mqtt.httpPort;
 
+MongoClient.connect(mqtt_mongodb_connection_string, {}, function (err, db) {
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
+    var settings = {
+        port: config.mqtt.port,
+        logger: {
+            level: config.mqtt.loglevel
+        },
+        persistence: {
+            connection: db,
+            factory: mosca.persistence.Mongo},
+        http: {
+            port: server_port,
+            static: __dirname + "/static",
+            bundle: true,
+            stats: false
+        },
+        onlyHttp: false
     };
 
+    var server = new mosca.Server(settings);
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+    if (config.auth0.enabled) {
+        //auth0 connection where all user/devices are registered.
+        var auth0 = new Auth0Mosca(config.auth0.endpoint, config.auth0.clientSecret);
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+        //Wire up authentication & authorization to mosca
+        server.authenticate = auth0.authenticate();
+        server.authorizePublish = auth0.authorizePublish();
+        server.authorizeSubscribe = auth0.authorizeSubscribe();
+    }
 
+    // Wire up time series database
+    var timeSeries = new timeSeriesStore({verbose: config.tsstore.verbose, db: ts_mongodb_connection_string});
+    server.published = timeSeries.publish();
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
+    server.on('clientConnected', function (client) {
+        console.log('client connected', client.id);
+    });
 
+    server.on('published', function (packet, client) {
+        if (client != undefined)
+            console.log('Event Published: ' + packet.payload + ' Client :' + client.id);
+    });
 
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
+    server.on('ready', setup);
 
+    // fired when the mqtt server is ready
+    function setup() {
+        console.log('Mosca server is up and running')
+    }
 
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+});
