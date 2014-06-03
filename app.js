ea4fdbbfb5d2ecb330d1058f2c6d25a846a37567
Jens-Ole Graulund <jensole@graulund.net>
2014-06-03 23:38:27 +0200

    wiring security and mongodb connections

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var mosca = require('mosca'),
     Auth0Mosca = require('auth0mosca'),
-    timeSeriesStore = require('ts-store'),
+    TimeSeriesStore = require('ts-store'),
     mongoose = require('mongoose'),
     config = require('./config.json'),
     uriUtil = require('mongodb-uri');
@@ -13,71 +13,59 @@ var options = {
             keepAlive: 1,
             connectTimeoutMS: 30000
         }
-    },
-    replset: {
-        socketOptions: {
-            keepAlive: 1,
-            connectTimeoutMS: 30000
-        }
     }
 };
 
-var mongodbUri = config.mqtt.dbConnection + config.mqtt.db,
-    mongooseUri = uriUtil.formatMongoose(mongodbUri);
+var mongodbTSUri = config.tsstore.dbConnection + config.tsstore.db,
+    mongooseTSUri = uriUtil.formatMongoose(mongodbTSUri),
+    mongodbMqttUri = config.mqtt.dbConnection + config.mqtt.db,
+    mongooseMqttUri = uriUtil.formatMongoose(mongodbMqttUri),
+    MQTTconnection = mongoose.createConnection(mongooseMqttUri, options),
+    TSconnection = mongoose.createConnection(mongooseTSUri, options);
 
-mongoose.connect(mongooseUri, options);
-var conn = mongoose.connection;
-
-conn.on('error', console.error.bind(console, 'connection error:'));
-
-conn.once('open', function () {
-    // Wait for the database connection to establish, then start the app.
-
-    var settings = {
-            port: config.mqtt.port,
-            logger: {
-                level: config.mqtt.loglevel
-            },
-            persistence: {
-                connection: conn.db,
-                factory: mosca.persistence.Mongo},
-            http: {
-                port: config.mqtt.httpPort,
-                static: __dirname + "/static",
-                bundle: true,
-                stats: false
-            },
-            onlyHttp: false
+var settings = {
+        port: config.mqtt.port,
+        logger: {
+            level: config.mqtt.loglevel
         },
-        server = new mosca.Server(settings),
-    // Wire up time series database
-        timeSeries = new TimeSeriesStore({verbose: config.tsstore.verbose}),
-        auth0 = new Auth0Mosca(config.domains);
+        persistence: {
+            connection: MQTTconnection.db,
+            factory: mosca.persistence.Mongo},
+        http: {
+            port: config.mqtt.httpPort,
+            static: __dirname + "/static",
+            bundle: true,
+            stats: false
+        },
+        onlyHttp: false
+    },
+    server = new mosca.Server(settings),
+// Wire up time series database
+    timeSeries = new TimeSeriesStore(TSconnection, {verbose: config.tsstore.verbose}),
+    auth0 = new Auth0Mosca(config.domains);
 
-    if (config.enableAuth) {
-        //Wire up authentication & authorization to mosca
-        server.authenticate = auth0.authenticate();
-        server.authorizePublish = auth0.authorizePublish();
-        server.authorizeSubscribe = auth0.authorizeSubscribe();
-    }
+if (config.enableAuth) {
+    //Wire up authentication & authorization to mosca
+    server.authenticate = auth0.authenticate();
+    server.authorizePublish = auth0.authorizePublish();
+    server.authorizeSubscribe = auth0.authorizeSubscribe();
+}
 
-    server.published = timeSeries.publish();
+server.published = timeSeries.publish();
 
-    server.on('clientConnected', function (client) {
-        console.log('client connected', client.id);
-    });
-
-    server.on('published', function (packet, client) {
-        if (client !== undefined) {
-            console.log('Event Published: ' + packet.payload + ' Client :' + client.id);
-        }
-    });
-
-    // fired when the mqtt server is ready
-    function setup() {
-        console.log('Mosca server is up and running');
-    }
-
-    server.on('ready', setup);
-
+server.on('clientConnected', function (client) {
+    console.log('client connected', client.id);
 });
+
+server.on('published', function (packet, client) {
+    if (client !== undefined) {
+        console.log('Event Published: ' + packet.payload + ' Client :' + client.id);
+    }
+});
+
+// fired when the mqtt server is ready
+function setup() {
+    console.log('Mosca server is up and running');
+}
+
+server.on('ready', setup);
